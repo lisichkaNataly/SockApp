3a4e873025170bd898e823d9b7f69c619fb3f66d
Izmaylova <Natalyatf@yandex.ru>
2023-01-15 23:01:07 +0500

    initial commit

---
+++ src/test/java/me/izm/sockapp/SockAppApplicationTests.java
@@ -0,0 +1,13 @@
+package me.izm.sockapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SockAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
